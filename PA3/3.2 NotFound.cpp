e1052bc7ec8b37c2178a7f58bf33eb1e336533fd
zhaozkTHU <zhaozk21@outlook.com>
2022-10-30 10:27:31 +0800

    3.2 report

--- PA3/3.2 NotFound.cpp
+++ PA3/3.2 NotFound.cpp
@@ -2,6 +2,7 @@
 #define MAX_CAPACITY 524288
 using namespace std;
 
+// 压缩内存占用
 class Bit {
     unsigned *content;
 public:
@@ -18,24 +19,15 @@ public:
             content[num / 32] |= (1 << (num % 32));
         }
     }
-    void init() {
-        for (int i = 0; i < MAX_CAPACITY; i++) content[i] = 0;
-    }
 };
 
-/// @param a 待取数组
-/// @param num 取第num位0或1
-inline int get_01(unsigned *a, unsigned num) {
-    return (a[num / 32] >> (num % 32)) & 1;
-}
-
 int main() {
-    unsigned *a = new unsigned[MAX_CAPACITY]();
+    Bit a;
     char c;
     unsigned len = 0;
     while (scanf("%c", &c) != EOF) {
-        if (c == '0') a[len / 32] += 0 << len % 32;
-        else if (c == '1') a[len / 32] += 1 << len % 32;
+        if (c == '0') a.set(len, 0);
+        else if (c == '1') a.set(len, 1);
         else break;
         len++;
     }
@@ -44,8 +36,9 @@ int main() {
         Bit exist;
         unsigned exists = 0;
 
+        // 计算第一个待验证答案
         unsigned answer = 0;
-        for (unsigned i = 0; i < answer_length; i++) answer += get_01(a, i) << (answer_length - i - 1);
+        for (unsigned i = 0; i < answer_length; i++) answer += a[i] << (answer_length - i - 1);
         if (exist[answer] == 0) {
             exist.set(answer, 1);
             exists += 1;
@@ -53,9 +46,11 @@ int main() {
         }
 
         for (unsigned start = 1; start <= len - answer_length; start++) {
+            // 由前一个答案递推
             answer &= ~(1 << (answer_length - 1));
             answer <<= 1;
-            answer += get_01(a, start + answer_length - 1);
+            answer += a[start + answer_length - 1];
+            
             if (exist[answer] == 0) {
                 exist.set(answer, 1);
                 exists += 1;
@@ -69,7 +64,6 @@ int main() {
                         printf("%d", (i >> (answer_length - j - 1)) & 1);
                     }
                     printf("\n");
-                    delete[] a;
                     return 0;
                 }
             }
